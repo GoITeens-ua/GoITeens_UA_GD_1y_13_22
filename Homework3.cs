0395e9b9f84d5ec3f3eab80bc55ce027c1f83580
DESKTOP-LHEL5BB\User <makarova2521@gmail.com>
2022-11-23 10:38:22 +0100

    Example of the homework for the last lesson

---
+++ Homework3.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework3
+{
+    internal class Homework3
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            string answer;
+            int randomNumber;
+
+            do
+            {
+                Console.WriteLine("Make your gues! Heads or tails? To End the game enter 'end'");
+
+                answer = Console.ReadLine();
+                randomNumber = random.Next(0, 2);
+
+                if (answer == "heads" || answer == "tails")
+                {
+                    if (randomNumber == 0 && answer == "tails")
+                    {
+                        Console.WriteLine("You win!");
+                    }
+                    else if (randomNumber == 1 && answer == "heads")
+                    {
+                        Console.WriteLine("You win!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("You lose!");
+                    }
+                }
+            }
+            while (answer != "end");
+        }
+    }
+}
